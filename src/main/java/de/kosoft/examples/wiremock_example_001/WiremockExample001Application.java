d260cada495071cac33788bc6445b9c380802b04
Tobias Koppe <tkoppe@vectronic-aerospace.com>
2022-08-05 09:01:25 +0200

    Initial commit

---
+++ src/main/java/de/kosoft/examples/wiremock_example_001/WiremockExample001Application.java
@@ -0,0 +1,13 @@
+package de.kosoft.examples.wiremock_example_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockExample001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiremockExample001Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/kosoft/examples/wiremock_example_001/WiremockExample001ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kosoft.examples.wiremock_example_001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiremockExample001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
